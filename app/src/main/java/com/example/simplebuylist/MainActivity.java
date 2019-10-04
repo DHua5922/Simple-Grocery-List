e4f0786136fc538532d3ec46c15b642e72eec8ba
Dylan Hua <hua.dylan@gmail.com>
2019-10-03 21:44:25 -0500

    Sent flag for adding item to page for editing and adding item

--- app/src/main/java/com/example/simplebuylist/MainActivity.java
+++ app/src/main/java/com/example/simplebuylist/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.simplebuylist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ADD = "Adding New Item";
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -19,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView addItemBtn = findViewById(R.id.btn_add_item);
+        addItemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ItemEdit.class);
+                intent.putExtra("Adding New Item", ItemEdit.FLAG_ADD);
+                startActivity(intent);
+            }
+        });
 
         RecyclerView itemList = findViewById(R.id.item_list);
         ViewModel viewModel = new ViewModel(getApplication());
