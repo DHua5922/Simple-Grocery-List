5e2f85c6407fc0a34a1567ec48f7696ae0679ecd
Dylan Hua <hua.dylan@gmail.com>
2019-10-12 23:22:47 -0500

    Used appropriate action bar for adding or editing item. Get indication that user is adding or editing item.

--- app/src/main/java/com/example/simplebuylist/ItemEdit.java
+++ app/src/main/java/com/example/simplebuylist/ItemEdit.java
@@ -5,28 +5,44 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
 
 public class ItemEdit extends AppCompatActivity {
 
     public static final int FLAG_ADD = 0;
     public static final int FLAG_EDIT = 1;
+    public static int FLAG = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_edit);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
 
         Intent intent = getIntent();
-        int flag = intent.getIntExtra(MainActivity.EXTRA_ADD, 0);
-
-        switch(flag) {
-            case 0:
+        FLAG = intent.getIntExtra(MainActivity.EXTRA_ADD, 0);
+        switch(FLAG) {
+            case FLAG_ADD:
+                Button btnAddItem = findViewById(R.id.btn_add_item);
                 break;
-            case 1:
+            case FLAG_EDIT:
+                //btnAddSaveItem.setText(R.string.btn_save_item_text);
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        if(FLAG == FLAG_ADD)
+            getMenuInflater().inflate(R.menu.menu_add_item, menu);
+        else
+            getMenuInflater().inflate(R.menu.menu_edit_item, menu);
+
+        return true;
     }
 }
