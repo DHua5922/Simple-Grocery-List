1b45d3b54d9385c45dc143b72b89d1fa95ae5649
Dylan Hua <hua.dylan@gmail.com>
2019-10-18 21:00:59 -0500

    Processed item to be added

--- app/src/main/java/com/example/simplebuylist/ItemEdit.java
+++ app/src/main/java/com/example/simplebuylist/ItemEdit.java
@@ -6,13 +6,18 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.Button;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class ItemEdit extends AppCompatActivity {
 
-    public static final int FLAG_ADD = 0;
-    public static final int FLAG_EDIT = 1;
-    public static int FLAG = -1;
+    private static int REQUEST = -1;
+    public static final String EXTRA_NAME = "package com.example.simplebuylist.EXTRA_NAME";
+    public static final String EXTRA_PRICE = "package com.example.simplebuylist.EXTRA_PRICE";
+    public static final String EXTRA_ISBOUGHT = "package com.example.simplebuylist.EXTRA_ISBOUGHT";
+    private ViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +28,61 @@ public class ItemEdit extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
 
-        Intent intent = getIntent();
-        FLAG = intent.getIntExtra(MainActivity.EXTRA_ADD, 0);
-        switch(FLAG) {
-            case FLAG_ADD:
-                Button btnAddItem = findViewById(R.id.btn_add_item);
-                break;
-            case FLAG_EDIT:
-                //btnAddSaveItem.setText(R.string.btn_save_item_text);
-                break;
-        }
+        Intent request = new Intent();
+        REQUEST = request.getIntExtra(MainActivity.EXTRA_ACTION_REQUEST, 0);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        if(FLAG == FLAG_ADD)
-            getMenuInflater().inflate(R.menu.menu_add_item, menu);
-        else
-            getMenuInflater().inflate(R.menu.menu_edit_item, menu);
+        MenuInflater menuInflater = getMenuInflater();
+        if(REQUEST == MainActivity.ADD_ITEM_REQUEST) {
+            menuInflater.inflate(R.menu.menu_add_item, menu);
+        }
+        else {
+            menuInflater.inflate(R.menu.menu_edit_item, menu);
+        }
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem selectedItem) {
+
+        try {
+            if (selectedItem.getItemId() == R.id.btn_add_item) {
+                addNote();
+                return true;
+            }
+        } catch (IllegalArgumentException e) {
+            Text.printMessage(this, "Invalid price");
+        }
+
+        return super.onOptionsItemSelected(selectedItem);
+    }
+
+    private void addNote() {
+        EditText nameInput = findViewById(R.id.name_input);
+        EditText priceInput = findViewById(R.id.price_input);
+        CheckBox isBoughtCheckBox = findViewById(R.id.checkbox_isBought);
+
+        String name = nameInput.getText().toString();
+        if(name.isEmpty()) {
+            Text.printMessage(this, "Name cannot be empty");
+        }
+        else {
+            double price = Double.parseDouble(priceInput.getText().toString());
+            final int IS_BOUGHT = 1;
+            final int NOT_BOUGHT = 0;
+            int isBought = isBoughtCheckBox.isChecked() ? IS_BOUGHT : NOT_BOUGHT;
+
+            Intent data = new Intent();
+            data.putExtra(EXTRA_NAME, name);
+            data.putExtra(EXTRA_PRICE, price);
+            data.putExtra(EXTRA_ISBOUGHT, isBought);
+
+            setResult(RESULT_OK, data);
+            finish();
+        }
     }
 }
