fa30f0a716cbd6edb1b02ca3dd01843d3b7d6100
Dylan Hua <hua.dylan@gmail.com>
2019-10-18 20:59:50 -0500

    Loaded each item display and added operations for adding item, getting highest order, and changing list

--- app/src/main/java/com/example/simplebuylist/ItemAdapter.java
+++ app/src/main/java/com/example/simplebuylist/ItemAdapter.java
@@ -4,35 +4,50 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemHolder> {
 
     private ArrayList<Item> itemList;
+    private ViewModel viewModel;
+    private MainActivity context;
+
+    public ItemAdapter(MainActivity context, ViewModel viewModel) {
+        this.context = context;
+        this.viewModel = viewModel;
+    }
 
     public static class ItemHolder extends RecyclerView.ViewHolder {
-        public CheckBox checkBox;
-        public TextView itemName;
-        public TextView itemPrice;
+        private CheckBox checkBox;
+        private TextView itemNameView;
+        private TextView itemPriceView;
 
         public ItemHolder(View itemDisplay) {
             super(itemDisplay);
+            this.checkBox = itemDisplay.findViewById(R.id.checkbox_isBought);
+            this.itemNameView = itemDisplay.findViewById(R.id.nameView);
+            this.itemPriceView = itemDisplay.findViewById(R.id.priceView);
+
         }
-    }
 
-    public ItemAdapter() {
-        this.itemList = new ArrayList<>();
-    }
+        public CheckBox getCheckBox() {
+            return checkBox;
+        }
 
-    public ItemAdapter(List<Item> itemList) {
-        this.itemList = new ArrayList<>();
-        this.itemList.addAll(itemList);
+        public TextView getItemNameView() {
+            return itemNameView;
+        }
+
+        public TextView getItemPriceView() {
+            return itemPriceView;
+        }
     }
 
     @NonNull
@@ -45,8 +60,18 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemHolder> {
     @Override
     public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
         //onBindCheckbox();
-        //onBindNameView();
-        //onBindPriceView();
+        onBindNameView(holder, position);
+        onBindPriceView(holder, position);
+    }
+
+    public void onBindNameView(ItemHolder holder, int position) {
+        TextView itemNameView = holder.getItemNameView();
+        itemNameView.setText(itemList.get(position).getName());
+    }
+
+    public void onBindPriceView(ItemHolder holder, int position) {
+        TextView itemPriceView = holder.getItemPriceView();
+        itemPriceView.setText("" + itemList.get(position).getPrice());
     }
 
     @Override
@@ -54,4 +79,22 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemHolder> {
         return itemList.size();
     }
 
+    public boolean add(Item item) throws ExecutionException, InterruptedException {
+        if(viewModel.insert(item)) {
+            itemList.add(item);
+            notifyItemInserted(getItemCount() - 1);
+            return true;
+        }
+
+        return false;
+    }
+
+    public int getHighestOrder() {
+        return itemList.size() == 0 ? 0 : itemList.get(itemList.size() - 1).getOrder();
+    }
+
+    public void setItemList(ArrayList<Item> itemList) {
+        this.itemList = itemList;
+        notifyDataSetChanged();
+    }
 }
