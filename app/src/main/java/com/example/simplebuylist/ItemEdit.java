f00ee4b050a4f2eb351c172fef32bf1b81a33df3
Dylan Hua <hua.dylan@gmail.com>
2019-10-20 15:08:07 -0500

    Code for getting and receiving id is more concise

--- app/src/main/java/com/example/simplebuylist/ItemEdit.java
+++ app/src/main/java/com/example/simplebuylist/ItemEdit.java
@@ -19,7 +19,6 @@ public class ItemEdit extends AppCompatActivity {
     private EditText nameInput;
     private EditText priceInput;
     private CheckBox isBoughtCheckBox;
-    private long id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +36,9 @@ public class ItemEdit extends AppCompatActivity {
         Intent request = getIntent();
         if(request.hasExtra(ItemAdapter.EXTRA_ITEM_ID)) {
             REQUEST = ItemAdapter.EDIT_ITEM_REQUEST;
-            loadItemContent();
+            nameInput.setText(request.getStringExtra(ItemAdapter.EXTRA_ITEM_NAME));
+            priceInput.setText(String.valueOf(request.getDoubleExtra(ItemAdapter.EXTRA_ITEM_PRICE, 0)));
+            isBoughtCheckBox.setChecked(request.getBooleanExtra(ItemAdapter.EXTRA_ITEM_IS_BOUGHT, false));
         } else {
             REQUEST = MainActivity.ADD_ITEM_REQUEST;
         }
@@ -94,23 +95,16 @@ public class ItemEdit extends AppCompatActivity {
             int isBought = isBoughtCheckBox.isChecked() ? IS_BOUGHT : NOT_BOUGHT;
 
             Intent data = new Intent();
-            if(REQUEST == ItemAdapter.EDIT_ITEM_REQUEST) {
-                data.putExtra(ItemAdapter.EXTRA_ITEM_ID, id);
-            }
             data.putExtra(ItemAdapter.EXTRA_ITEM_NAME, name);
             data.putExtra(ItemAdapter.EXTRA_ITEM_PRICE, price);
             data.putExtra(ItemAdapter.EXTRA_ITEM_IS_BOUGHT, isBought);
+            long id = getIntent().getLongExtra(ItemAdapter.EXTRA_ITEM_ID, -1);
+            if(id != -1) {
+                data.putExtra(ItemAdapter.EXTRA_ITEM_ID, id);
+            }
 
             setResult(RESULT_OK, data);
             finish();
         }
-    }
-
-    private void loadItemContent() {
-        Intent request = getIntent();
-        id = request.getLongExtra(ItemAdapter.EXTRA_ITEM_ID, 0);
-        nameInput.setText(request.getStringExtra(ItemAdapter.EXTRA_ITEM_NAME));
-        priceInput.setText(String.valueOf(request.getDoubleExtra(ItemAdapter.EXTRA_ITEM_PRICE, 0)));
-        isBoughtCheckBox.setChecked(request.getBooleanExtra(ItemAdapter.EXTRA_ITEM_IS_BOUGHT, false));
     }
 }
