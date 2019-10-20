6d129e027716081be93064afb8b4e5c35fde8927
Dylan Hua <hua.dylan@gmail.com>
2019-10-19 22:32:19 -0500

    Added operation for formatting item price in item display

--- app/src/main/java/com/example/simplebuylist/Text.java
+++ app/src/main/java/com/example/simplebuylist/Text.java
@@ -3,10 +3,16 @@ package com.example.simplebuylist;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class Text {
 
     public static void printMessage(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    public static String formatPrice(double price) {
+        return String.format(Locale.getDefault(), "Price: $%.2f", price);
     }
 
 }
