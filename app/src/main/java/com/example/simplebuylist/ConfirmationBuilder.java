a42d49f1f53bd65b4776ce91c8f086bfdbefb835
Dylan Hua <hua.dylan@gmail.com>
2019-11-17 20:08:05 -0600

    Created confirmation dialog.

---
+++ app/src/main/java/com/example/simplebuylist/ConfirmationBuilder.java
@@ -0,0 +1,11 @@
+package com.example.simplebuylist;
+
+import android.content.Context;
+
+public class ConfirmationBuilder extends DialogBuilder {
+
+    public ConfirmationBuilder(Context context, int dialogLayout) {
+        super(context, dialogLayout);
+    }
+
+}
