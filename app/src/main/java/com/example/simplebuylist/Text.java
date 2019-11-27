f44d273c9fe9911e176d58f4260f2d40067cf770
Dylan Hua <hua.dylan@gmail.com>
2019-11-26 18:39:03 -0600

    Included image that indicates successful or failed operation to left of message

--- app/src/main/java/com/example/simplebuylist/Text.java
+++ app/src/main/java/com/example/simplebuylist/Text.java
@@ -1,14 +1,30 @@
 package com.example.simplebuylist;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
 
 public class Text {
 
-    public static void printMessage(Context context, String message) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    public static final int SUCCESS = 1;
+    public static final int FAIL = 0;
+
+    public static void printMessage(Context context, String message, int status) {
+        View layout = LayoutInflater.from(context).inflate(R.layout.toast, null);
+
+        TextView toastMessage = layout.findViewById(R.id.toast_msg);
+        int imgId = (status == SUCCESS) ? R.drawable.icon_check : R.drawable.icon_fail;
+        toastMessage.setCompoundDrawablePadding(20);
+        toastMessage.setCompoundDrawablesWithIntrinsicBounds(imgId, 0, 0, 0);
+        toastMessage.setText(message);
+
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
     }
 
     public static String formatPrice(double price) {
