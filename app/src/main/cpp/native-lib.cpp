be175d32e0f5a9c7390c55965c9568e29a38b6f6
Dylan Hua <hua.dylan@gmail.com>
2019-11-28 14:56:47 -0600

    Calculated total price of all given items

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,19 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_simplebuylist_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C"
+JNIEXPORT jdouble JNICALL
+Java_com_example_simplebuylist_MainActivity_getTotalPrice(JNIEnv *env, jobject thiz,
+                                                          jobjectArray item_array) {
+    double totalPrice = 0.0;
+    int len = env->GetArrayLength(item_array);
+
+    jclass jcls = env->FindClass("com/example/simplebuylist/Item");
+    jmethodID mID = env->GetMethodID(jcls, "getPrice", "()D");
+    for (int i = 0; i < len; i++) {
+        auto obj = (jobject) env->GetObjectArrayElement(item_array, i);
+        totalPrice += env->CallDoubleMethod(obj, mID);
+    }
+
+    return totalPrice;
 }
