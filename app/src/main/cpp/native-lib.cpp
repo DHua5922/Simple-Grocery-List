5ed6169ab92403f18d5d2958a70b929f6d2642ca
Dylan Hua <hua.dylan@gmail.com>
2019-11-29 13:11:07 -0600

    Updated function with updated package name

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,12 +3,12 @@
 
 extern "C"
 JNIEXPORT jdouble JNICALL
-Java_com_example_simplebuylist_MainActivity_getTotalPrice(JNIEnv *env, jobject thiz,
+Java_com_dylanhua_simplebuylist_MainActivity_getTotalPrice(JNIEnv *env, jobject thiz,
                                                           jobjectArray item_array) {
     double totalPrice = 0.0;
     int len = env->GetArrayLength(item_array);
 
-    jclass jcls = env->FindClass("com/example/simplebuylist/Item");
+    jclass jcls = env->FindClass("com/dylanhua/simplebuylist/Item");
     jmethodID mID = env->GetMethodID(jcls, "getPrice", "()D");
     for (int i = 0; i < len; i++) {
         auto obj = (jobject) env->GetObjectArrayElement(item_array, i);
